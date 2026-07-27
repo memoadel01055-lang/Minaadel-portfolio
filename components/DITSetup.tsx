6e6058f66de2dc0c567dc96f0dcd0b0e5e28f1b7
mina <mac@macs-mina-MacBook-Pro.local>
2026-07-27 11:44:39 +0300

    Update showreel mix videos

--- components/DITSetup.tsx
+++ components/DITSetup.tsx
@@ -11,7 +11,6 @@ const ditImages = [
   "/images/dit setup/ultraStudio.jpg",
   "/images/dit setup/3b6d386.jpg",
   "/images/dit setup/IMG_2503 2.jpg",
-  "/images/dit setup/DSC0384.jpg",
   "/images/dit setup/Screenshot.jpg",
   "/images/dit setup/Screenshot 20.jpg",
   "/images/dit setup/Screenshot 2026.jpg",
